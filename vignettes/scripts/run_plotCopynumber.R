563335090518c555823bd2758e051bee19699571
Shadi Shafighi <dsh.shadi@gmail.com>
2023-04-04 14:51:01 +0200

    updating plotting script to get args

--- vignettes/scripts/run_plotCopynumber.R
+++ vignettes/scripts/run_plotCopynumber.R
@@ -1,8 +1,14 @@
 library(QDNAseq)
-BASEDIR="~/Projects/scAbsoluteMatters/scAbsolute"
+args = commandArgs(trailingOnly=TRUE)
+
+BASEDIR= args[1]
+#BASEDIR="~/Projects/scAbsoluteMatters/scAbsolute"
+
 source(file.path(BASEDIR, "R/core.R"))
 source(file.path(BASEDIR, "R/visualization.R"))
-CN = readRDS("~/Projects/scAbsoluteMatters/results/500/individual/UID-10X-Fibroblast-cell_SLX-00000_000001_AAACCTGAGCAGCCTC-1.rds")
+
+CN = readRDS(args[2])
+#CN = readRDS("~/Projects/scAbsoluteMatters/results/500/individual/UID-10X-Fibroblast-cell_SLX-00000_000001_AAACCTGAGCAGCCTC-1.rds")
 
 # plot single cell copy number profile
 plotCopynumber(CN[, 1])
