89b20e69221fb35eb030df1e715060099cfad64e
leachim <leachim@users.noreply.github.com>
2024-04-04 14:00:07 +0100

    add vignette for ploidy estimation/WGS

--- vignettes/compile-vignettes.R
+++ vignettes/compile-vignettes.R
@@ -3,5 +3,8 @@ library(knitr)
 rmarkdown::render("vignette-basics.Rmd", output_file="vignette-basics.html")
 rmarkdown::render("vignette-basics.Rmd", output_file="vignette-basics.pdf")
 
+rmarkdown::render("vignette-ploidy", output_file="vignette-ploidy")
+rmarkdown::render("vignette-ploidy", output_file="vignette-ploidy")
+
 rmarkdown::render("vignette-rCNAs.Rmd", output_file="vignette-rCNAs.html")
 rmarkdown::render("vignette-rCNAs.Rmd", output_file="vignette-rCNAs.pdf")
