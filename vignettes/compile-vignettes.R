430d7a7bde957085d52dd25a2dde35305f680ca9
leachim <leachim@users.noreply.github.com>
2024-04-04 14:26:56 +0100

    new vignette for ploidy

--- vignettes/compile-vignettes.R
+++ vignettes/compile-vignettes.R
@@ -3,8 +3,8 @@ library(knitr)
 rmarkdown::render("vignette-basics.Rmd", output_file="vignette-basics.html")
 rmarkdown::render("vignette-basics.Rmd", output_file="vignette-basics.pdf")
 
-rmarkdown::render("vignette-ploidy", output_file="vignette-ploidy")
-rmarkdown::render("vignette-ploidy", output_file="vignette-ploidy")
+rmarkdown::render("vignette-ploidy.Rmd", output_file="vignette-ploidy.html")
+rmarkdown::render("vignette-ploidy.Rmd", output_file="vignette-ploidy.pdf")
 
 rmarkdown::render("vignette-rCNAs.Rmd", output_file="vignette-rCNAs.html")
 rmarkdown::render("vignette-rCNAs.Rmd", output_file="vignette-rCNAs.pdf")
